04de590199544c47600c158e0d64d0acdfbc4b18
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-06-21 19:33:28 +0100

    Update ApplicantsController.cs

--- Hahn.ApplicatonProcess.May2020.Web/Controllers/ApplicantsController.cs
+++ Hahn.ApplicatonProcess.May2020.Web/Controllers/ApplicantsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hahn.ApplicatonProcess.May2020.Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,10 +12,12 @@ namespace Hahn.ApplicatonProcess.May2020.Web.Controllers
     public class ApplicantsController : ControllerBase
     {
         private readonly ILogger<ApplicantsController> _logger;
+        private IApplicantService _applicantService;
 
-        public ApplicantsController(ILogger<ApplicantsController> logger)
+        public ApplicantsController(ILogger<ApplicantsController> logger, IApplicantService applicantService)
         {
             _logger = logger;
+            _applicantService = applicantService;
         }
 
         [HttpGet("/api/applicants")]
