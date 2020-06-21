12df4d73317bd285a477d8a4bc58bbf9e4f64bff
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-06-21 19:49:36 +0100

    Update ApplicantsController.cs

--- Hahn.ApplicatonProcess.May2020.Web/Controllers/ApplicantsController.cs
+++ Hahn.ApplicatonProcess.May2020.Web/Controllers/ApplicantsController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hahn.ApplicatonProcess.May2020.Data.Models;
 using Hahn.ApplicatonProcess.May2020.Domain;
+using Hahn.ApplicatonProcess.May2020.Web.RequestModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -25,6 +27,26 @@ namespace Hahn.ApplicatonProcess.May2020.Web.Controllers
         {
             var applicants = _applicantService.GetAllApplicants();
             return Ok(applicants);
+        }
+
+        [HttpPost("/api/applicants")]
+        public ActionResult CreateApplicant([FromBody] NewApplicantRequest applicantRequest)
+        {
+            var now = DateTime.UtcNow;
+            var applicant = new Applicant
+            {
+                CreatedOn = now,
+                UpdatedOn = now,
+                Name = applicantRequest.Name,
+                Surname = applicantRequest.Surname,
+                Country = applicantRequest.Country,
+                Address = applicantRequest.Address,
+                Age = applicantRequest.Age,
+                Email = applicantRequest.Email,
+                Hired = applicantRequest.Hired,
+            };
+            _applicantService.AddApplicant(applicant);
+            return Ok($"Applicant Created: {applicant.Name}");
         }
     }
 }
